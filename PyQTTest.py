c1d85fb63acee8d9bbee5a07c7ef70f88e9ea5f6
johns-del <yyl976079@gmail.com>
2025-04-25 13:18:42 +0800

    QT Test

---
+++ PyQTTest.py
@@ -0,0 +1,9 @@
+import PySide6
+from PySide6 import QtCore
+import sys
+print(f"PySide6 version: {PySide6.__version__}")
+# 也可以检查 Qt 版本
+print(f"Qt version: {QtCore.__version__}")
+# 检查 Python 版本
+print(f"Python version: {sys.version}")
+
